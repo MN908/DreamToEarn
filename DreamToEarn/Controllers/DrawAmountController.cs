aa779ee3c45d2acf51316f2b660b96d55e43d50f
Muhammad Nasir <muhammad.nasir.khan@codingkey.com>
2023-07-22 19:21:32 +0500

    Imag

---
+++ DreamToEarn/Controllers/DrawAmountController.cs
@@ -0,0 +1,296 @@
+﻿using DreamToEarn.DbModel;
+using DreamToEarn.Helper;
+using DreamToEarn.Models;
+using DreamToEarn.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DreamToEarn.Controllers
+{
+    public class DrawAmountController : AppController
+    {
+        // GET: DrawAmount
+        [HttpGet]
+        [Authorize(Roles = "1,2,3")]
+        public ActionResult Withdraw()
+        {
+            var date = DateTime.Now;
+            var days = WithDrawRepository.GetAllWithDrawDays();
+            ViewBag.isWithdrawDate = "false";
+
+            if (date.Day == days.FirstDay)
+                ViewBag.isWithdrawDate = "true";
+            if (date.Day == days.SecondDay)
+                ViewBag.isWithdrawDate = "true";
+            if (date.Day == days.ThirdDay)
+                ViewBag.isWithdrawDate = "true";
+            if (date.Day == days.ForthDay)
+                ViewBag.isWithdrawDate = "true";
+
+            ViewBag.dayOne = days.FirstDay;
+            ViewBag.dayTwo = days.SecondDay;
+            ViewBag.dayThree = days.ThirdDay;
+            ViewBag.dayFour = days.ForthDay;
+
+            var earnings = ViewBag.TotalEarnings = InvestmentPlanRepository.TotalEarnings(CurrentUser.UserId);
+            var commisions = ViewBag.TotalCommissions = CommissionRepository.GetUserApprovedCommision(CurrentUser.UserId);
+            if (earnings == null)
+            {
+                earnings = 0;
+                ViewBag.TotalEarnings = 0;
+            }
+            if (commisions == null)
+            {
+                commisions = 0;
+                ViewBag.TotalCommissions = 0;
+            }
+
+            if (earnings > 0 || commisions > 0)
+                ViewBag.TotalBalance = Convert.ToDecimal(earnings) + Convert.ToDecimal(commisions);// earning should be commission + earnings    
+            else
+                ViewBag.TotalBalance = 0;
+
+            var usersDetail = UsersRepository.GetUser(CurrentUser.UserId);
+            ViewBag.BankAccount = usersDetail.BankAccount.ToDefaultString();
+            ViewBag.JazzCash = usersDetail.JazzCash.ToDefaultString();
+            ViewBag.EasyPaisa = usersDetail.EasyPaisa.ToDefaultString();
+            ViewBag.BankTitle = usersDetail.BankTitle;
+
+            var ex = UsersRepository.GetAllExchanger();
+            ViewBag.ExchangerName = $"{ex.FirstName} {ex.LastName}";
+
+
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "1,2,3")]
+        public JsonResult Withdraw(WithdrawModel model)
+        {
+            try
+            {
+                var obj = UsersRepository.GetUser(model.UserId);
+                if (model.drawType == "BankAccount")
+                {
+                    if (obj.BankAccount.ToDefaultString() == "")
+                        return Json(new { IsSuccess = false, Message = "Your Bank Account detail is missing, please update your profle before withdraw." }, JsonRequestBehavior.AllowGet);
+                }
+                if (model.drawType == "JazzCash")
+                {
+                    if (obj.JazzCash.ToDefaultString() == "")
+                        return Json(new { IsSuccess = false, Message = "Your Jazz Cash number is missing, please update your profle before withdraw." }, JsonRequestBehavior.AllowGet);
+                }
+                if (model.drawType == "EasyPaisa")
+                {
+                    if (obj.EasyPaisa.ToDefaultString() == "")
+                        return Json(new { IsSuccess = false, Message = "Your Easy Paisa number is missing, please update your profle before withdraw." }, JsonRequestBehavior.AllowGet);
+                }
+
+                if (WithDrawRepository.IsWithdrawPending(model.UserId) == false)
+                {
+                    return Json(new { IsSuccess = false, Message = "Your transaction is already in Pending state. You can not withdraw." }, JsonRequestBehavior.AllowGet);
+                }
+                if (model.totalDraw >= model.totalEarningOnDraw)
+                {
+                    return Json(new { IsSuccess = false, Message = "Draw amount should be less than then total balance." }, JsonRequestBehavior.AllowGet);
+                }
+
+                var deduction = (model.totalDraw * 2.75) / 100;
+                var grandTotalDraw = model.totalDraw + deduction;
+
+                if (grandTotalDraw > model.totalEarningOnDraw) // if total draw + deduction is greater than total balance
+                {
+                    return Json(new { IsSuccess = false, Message = "Please enter less withdraw amount, your balance is insufficient for this request." }, JsonRequestBehavior.AllowGet);
+                }
+                model.deductions = Convert.ToDecimal(deduction);
+
+                Random random = new Random();
+                int OTPToken = random.Next(10000000);
+
+                model.OTPTokens = OTPToken.ToDefaultString();
+
+                var email = new EmailHelper();
+                email.emailToAddress = CurrentUser.Email;
+                email.subject = "Dream to Earn First Login Token";
+                email.body = $"Dear User, <br> <br> Please copy following OTP for your withdraw transaction. <br> <br> Your OTP is <b>{OTPToken}</b> <br><br> Please do not delete this token before withdraw. <br> <br> This is an auto email, please do not reply.";
+                email.SendEmail();
+
+                var draw = WithDrawRepository.WithDrawAmount(model);
+
+
+
+                return Json(new { IsSuccess = true, Message = "OTP sent" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { IsSuccess = false, Message = "Amount with draw failed" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        [Authorize(Roles = "1,2,3")]
+        public JsonResult VerifyOTPTokens(WithdrawModel model)
+        {
+            try
+            {
+
+                if (WithDrawRepository.VarifyOTPToken(model.OTPTokens, model.UserId))
+                {
+                    //var deduction = Math.Round((model.totalDraw * 2.75) / 100, 2, MidpointRounding.ToEven);
+
+                    //var totalBalance = model.totalEarningOnDraw;
+
+                    //var totalLeftBalance = totalBalance - model.totalDraw - deduction; //3200 - 3000 - 82.5 = 1717.5
+                    //                                                                   //now 82.5 should be added in admin balance; and 1717.5 should be updated to investor commission or earnings
+
+                    //var earnings = model.earnings; //2000
+                    //var commissions = model.commissions; //1200                
+
+                    model.drawDate = DateTime.Now;
+                    model.drawStatus = true;
+                    model.isAdminApprove = false;
+
+                    WithDrawRepository.UpdateWithdrawStatus(model);
+                    //make earning zero and add remaining balance to commission
+
+                    //CommissionRepository.DeductCommissions(model.UserId, Convert.ToDecimal(totalLeftBalance));
+                    //InvestmentPlanRepository.DeductEarnings(model.UserId, 0);
+
+
+                    //UsersRepository.UpdateTotalCommissionsForAdmin(Convert.ToDecimal(deduction));
+
+                    return Json(new { IsSuccess = true, Message = "Withdraw successfull, OTP matched" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                    return Json(new { IsSuccess = false, Message = "Withdraw failed, OTP not matched" }, JsonRequestBehavior.AllowGet);
+
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { IsSuccess = false, Message = "Withdraw failed" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "1,3")]
+        public ActionResult ApproveWithdraw()
+        {
+            if (CurrentUser.Role == "1")
+                return View(WithDrawRepository.GetAllWithDraws());
+            else
+                return View(WithDrawRepository.GetAllWithDrawsForExchanger());
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "2, 3")]
+        public ActionResult ViewWithDraw()
+        {
+            return View(WithDrawRepository.GetUserWithDraws(CurrentUser.UserId));
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "1")]
+        public ActionResult setDrawDates()
+        {
+            var days = WithDrawRepository.GetAllWithDrawDays();
+            ViewBag.dayOne = days.FirstDay;
+            ViewBag.dayTwo = days.SecondDay;
+            ViewBag.dayThree = days.ThirdDay;
+            ViewBag.dayFour = days.ForthDay;
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "1")]
+        public ActionResult drawHistory()
+        {
+            var totalYTDWithdraw = WithDrawRepository.getTotalYTDApprovedWithdraws();
+            var lastWithDraw = WithDrawRepository.getLastApprovedWithdraws();
+            ViewBag.totalYTDWithdraw = totalYTDWithdraw;
+            ViewBag.lastWithDraw = lastWithDraw;  
+            
+            return View(WithDrawRepository.GetAllApprovedWithDraws());
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "1")]
+        public JsonResult setDrawDates(int dayOne, int dayTwo, int dayThree, int dayFour)
+        {
+            try
+            {
+                WithDrawRepository.addDrawDays(dayOne, dayTwo, dayThree, dayFour);
+
+                return Json(new { IsSuccess = true, Message = "Withdraw days added successfull" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { IsSuccess = false, Message = "Adding Withdraw days fail" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "1,3")]
+        public JsonResult ApproveWithdraw(int drawId, int UserId)
+        {
+            try
+            {
+                var model = WithDrawRepository.GetAllWithDraws(drawId);
+
+                //var deduction = Math.Round((Convert.ToDouble(model.totalDraw) * 2.75) / 100, 2, MidpointRounding.ToEven);
+
+                //var totalBalance = Convert.ToDouble(model.totalEarningOnDraw);
+
+                //var totalLeftBalance = totalBalance - Convert.ToDouble(model.totalDraw) - deduction; //3200 - 3000 - 82.5 = 1717.5
+                //now 82.5 should be added in admin balance; and 1717.5 should be updated to investor commission or earnings
+                //make earning zero and add remaining balance to commission
+
+                WithDrawRepository.ApproveWithdrawStatus(drawId);
+                //CommissionRepository.DeductCommissions(UserId, Convert.ToDecimal(totalLeftBalance));
+                //InvestmentPlanRepository.DeductEarnings(UserId, 0);
+                //UsersRepository.UpdateTotalCommissionsForAdmin(Convert.ToDecimal(deduction));
+
+                return Json(new { IsSuccess = true, Message = "Withdraw approved successfull" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { IsSuccess = false, Message = "Withdraw approved failed" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        [Authorize(Roles = "1,3")]
+        public JsonResult ApproveExchangerWithdraw(int drawId, int UserId)
+        {
+            try
+            {
+                var model = WithDrawRepository.GetAllWithDraws(drawId);
+
+                var UserDetail = WithDrawRepository.GetUserDetail(model.UserId);
+
+                var deduction = Math.Round((Convert.ToDouble(model.totalDraw) * 2.75) / 100, 2, MidpointRounding.ToEven);
+
+                var totalBalance = Convert.ToDouble(UserDetail.TotalEarnings) + Convert.ToDouble(UserDetail.TotalCommissionsx); // Current balance;
+
+                var totalLeftBalance = totalBalance - Convert.ToDouble(model.totalDraw) - deduction; //3200 - 3000 - 82.5 = 1717.5
+                                                                                                     //now 82.5 should be added in admin balance; and 1717.5 should be updated to investor commission or earnings
+
+                //make earning zero and add remaining balance to commission
+
+                WithDrawRepository.ApproveWithdrawStatusForExchanger(drawId);
+                CommissionRepository.DeductCommissions(UserId, Convert.ToDecimal(totalLeftBalance));
+                InvestmentPlanRepository.DeductEarnings(UserId, 0);
+
+
+                UsersRepository.UpdateTotalCommissionsForAdmin(Convert.ToDecimal(deduction));
+
+                return Json(new { IsSuccess = true, Message = "Withdraw approved successfull" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { IsSuccess = false, Message = "Withdraw approved failed" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ DreamToEarn/Repositories/UsersRepository.cs
@@ -0,0 +1,362 @@
+﻿using DreamToEarn.DbModel;
+using DreamToEarn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DreamToEarn.Helper;
+
+namespace DreamToEarn.Repositories
+{
+    public class UsersRepository
+    {
+        public static bool Login(string Email, string Pass)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+
+                var obj = context.Users.FirstOrDefault(x => x.Email.Equals(Email) && x.Password.Equals(Pass, StringComparison.InvariantCulture));
+                if (obj != null)
+                    return true;
+
+                return false;
+            }
+        }
+        public static bool VarifyToken(string Token, int userId)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.firstLoginToken.Equals(Token) && x.UserId == userId);
+                if (obj != null)
+                    return true;
+
+                return false;
+            }
+        }
+
+        public static bool userHaveReference(int userId)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.UserId == userId && x.RefUserId > 0);
+                if (obj != null)
+                    return true;
+
+                return false;
+            }
+        }
+
+        public static User UpdateUserFirstLogin(int userId)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.UserId == userId);
+                if (obj != null)
+                {
+                    obj.firstLogin = false;
+                }
+                context.SaveChanges();
+
+                return obj;
+
+            }
+        }
+
+        public static User UpdateTotalCommissionsForAdmin(decimal? commission)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.Role == 1 && x.SuperAdmin == true);
+                if (obj != null)
+                {
+                    obj.TotalCommissions += commission;
+                }
+                context.SaveChanges();
+
+                return obj;
+
+            }
+        }
+
+        public static User UpdateTotalCommissions(int userId, decimal? totalCommission)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.UserId == userId);
+                if (obj != null)
+                {
+                    obj.TotalCommissions = totalCommission;
+                }
+                context.SaveChanges();
+
+                return obj;
+
+            }
+        }
+
+        public static User GetUser(string Email)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.Email.Equals(Email));
+                if (obj != null)
+                    return obj;
+
+                return obj;
+            }
+        }
+        public static User GetUser(int userId)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+
+                var obj = context.Users.FirstOrDefault(x => x.UserId == userId);
+                if (obj != null)
+                    return obj;
+
+                return obj;
+            }
+        }
+        public static User RegisterUser(RegisterModel user)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+
+                var obj = new User();
+
+                obj.UserName = user.UserName;
+                obj.FirstName = user.FirstName;
+                obj.Email = user.EmailAddress;
+                obj.LastName = user.LastName;
+                obj.Password = user.Password.Encrypt();
+                obj.RefUserId = user.RefUserId;
+                obj.Role = 2;
+                obj.firstLogin = true;
+                obj.firstLoginToken = user.firstLoginToken;
+                context.Users.Add(obj);
+
+                context.SaveChanges();
+
+                return obj;
+
+            }
+        }
+
+        public static User UpdateUserDetail(RegisterModel user)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.UserId == user.userId);
+                if (obj != null)
+                {
+                    obj.FirstName = user.FirstName;                    
+                    obj.LastName = user.LastName;
+                    obj.UserAddress = user.Address;
+                    obj.Mobile = user.Mobile;
+                    obj.BankAccount = user.BankAccount;
+                    obj.BankTitle = user.BankTitle;
+                    obj.JazzCash = user.JazzCash;
+                    obj.EasyPaisa = user.EasyPaisa;
+                }
+                context.SaveChanges();
+
+                return obj;
+
+            }
+        }
+
+        public static User SetExchangerUser(int userId)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.UserId == userId);
+                if (obj != null)
+                {
+                    obj.Role = 3;
+                }
+                context.SaveChanges();
+
+                return obj;
+
+            }
+        }
+
+        public static bool isExchangerAlreadySet()
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.Role == 3);
+                if (obj != null)
+                    return true;
+
+                return false;
+            }
+        }
+        public static User RemoveExchangerUser(int userId)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.UserId == userId);
+                if (obj != null)
+                {
+                    obj.Role = 2;
+                }
+                context.SaveChanges();
+
+                return obj;
+
+            }
+        }
+
+        public static bool oldPasswordMatched(ChangePasswordModel pass)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                string password = pass.oldPassword.Encrypt();
+                var obj = context.Users.FirstOrDefault(x => x.UserId == pass.userId && x.Password.Equals(password, StringComparison.InvariantCulture));
+                if (obj != null)
+                    return true;
+
+                return false;
+
+            }
+        }
+
+        public static User ChangePassword(ChangePasswordModel pass)
+        {
+            using (var context = new DreamToEarn.DbModel.DreamToEarnEntities1())
+            {
+                string password = pass.oldPassword.Encrypt();
+                var obj = context.Users.FirstOrDefault(x => x.UserId == pass.userId && x.Password.Equals(password, StringComparison.InvariantCulture));
+
+                if (obj != null)
+                {
+                    obj.Password = pass.newPassword.Encrypt();
+                }
+                context.SaveChanges();
+
+                return obj;
+
+            }
+        }
+
+        public static bool UserNameAlreadyExist(string UserName)
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.UserName == UserName);
+                if (obj != null)
+                    return true;
+                else
+                    return false;
+            }
+
+        }
+
+        public static bool EmailAlreadyExist(string Email)
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.Email.Equals(Email));
+                if (obj != null)
+                    return true;
+                else
+                    return false;
+            }
+
+        }
+
+        public static int GetIDAgainstUserName(string UserName)
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.UserName == UserName);
+                if (obj != null)
+                    return obj.UserId;
+                else
+                    return 0;
+            }
+
+        }
+        public static List<Team> GetTeam(int userId)
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                return context.Teams.Where(x => x.RefUserId == userId).OrderBy(x => x.FirstName).ThenBy(x => x.LastName).ToList();
+            }
+
+        }
+        public static List<User> GetAllTeam()
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                return context.Users.Where(x => x.Email != "kumail.mehdi001@gmail.com" && x.Role != 1 && x.Role != 3).OrderBy(x => x.FirstName).ThenBy(x => x.LastName).ToList();
+            }
+
+        }
+
+        public static List<User> GetAllExchangers()
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                return context.Users.Where(x => x.Email != "kumail.mehdi001@gmail.com" && x.Role == 3 && x.Role != 1).OrderBy(x => x.FirstName).ThenBy(x => x.LastName).ToList();
+            }
+
+        }
+
+        public static User GetAllExchanger()
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                return context.Users.FirstOrDefault(x => x.Email != "kumail.mehdi001@gmail.com" && x.Role == 3 && x.Role != 1);
+            }
+
+        }
+
+        public static List<Team> GetAllTeam(int RefUserId)
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                return context.Teams.Where(x => x.RefUserId == RefUserId && x.RefUserId != 0 && x.Email != "kumail.mehdi001@gmail.com").OrderBy(x => x.FirstName).ThenBy(x => x.LastName).ToList();
+            }
+
+        }
+
+        public static int TotalTeamMembers(int userId)
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                var obj = context.Teams.Where(x => x.RefUserId == userId).OrderBy(x => x.FirstName).ThenBy(x => x.LastName);
+                if (obj != null)
+                    return obj.Count();
+
+                return 0;
+            }
+
+        }
+        public static decimal TotalInvestmentPlan(int userId)
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                var obj = context.InvestmentPlans.FirstOrDefault(x => x.UserId == userId && x.IsActive == true);
+                if (obj != null)
+                    return obj.plan.ToDecimal();
+
+                return 0;
+            }
+
+        }
+
+        public static int? GetRefUserId(int userId)
+        {
+            using (var context = new DreamToEarnEntities1())
+            {
+                var obj = context.Users.FirstOrDefault(x => x.UserId == userId);
+                if (obj != null)
+                    return obj.RefUserId;
+                return 0;
+            }
+
+        }
+    }
+}
